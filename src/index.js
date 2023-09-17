a3314453f247c1966f1fb3c82f9b66ca23dc6602
syovcheva <s.yovcheva03@gmail.com>
2023-09-17 15:41:27 +0300

    Add Express project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello from express');
+const express = require('express');
+
+const app = express();
+const PORT = 8000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from express');
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
