3953ec33676566280e8ae07edc441c029b499238
syovcheva <s.yovcheva03@gmail.com>
2023-09-24 13:03:11 +0300

    Add 404 page

--- src/index.js
+++ src/index.js
@@ -14,6 +14,9 @@ expressConfig(app);
 handlebarsConfig(app);
 // Routes
 app.use(homeController); 
-app.use('/cubes', cubeController); 
+app.use('/cubes', cubeController);
+app.get('*', (req, res) => {
+    res.redirect('/404')
+})
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
