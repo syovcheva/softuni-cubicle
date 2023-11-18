0fc034394bd5490f4e8490fd37196f2bdb39a545
syovcheva <s.yovcheva03@gmail.com>
2023-11-18 18:02:13 +0200

    Hash password before save

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: String,
@@ -19,6 +20,11 @@ userSchema.virtual('repeatPassword')
             throw new mongoose.MongooseError('Password missmatch!');
         }
     });
+
+    userSchema.pre('save', async function() {
+        const hash = await bcrypt.hash(this.password, 10);
+        this.password = hash;
+    })
 
 const User = mongoose.model('User', userSchema);
 
