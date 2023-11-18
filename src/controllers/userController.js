55b78bf2be23fd7dc813cf8226f7063d44ab429f
syovcheva <s.yovcheva03@gmail.com>
2023-11-18 18:06:27 +0200

    Add login page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,6 +12,10 @@ router.post('/register', async (req, res) => {
     await userManager.register({username, password, repeatPassword });
 
     res.redirect('/users/login');
-})
+});
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
 
 module.exports = router;
